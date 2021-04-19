97a78d20da66b4aee56e3c8b002ff80ae7341dbc
Ashish <mhrj.asis@gmail.com>
2021-04-19 04:52:19 +0200

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# nepalese-food
+# MO:MO: Ristorante
+
+MO:MO: Ristorante is a restaurant website.
+
+## v1.0.
+
+![screenshots/screenshot-v1.0.png]
+
--- dev/momo_ristorante/momo_ristorante/settings.py
+++ dev/momo_ristorante/momo_ristorante/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,8 +41,6 @@ INSTALLED_APPS = [
     'website',
     'momos',
     'blogs',
-    'api.apps.ApiConfig',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +58,7 @@ ROOT_URLCONF = 'momo_ristorante.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
